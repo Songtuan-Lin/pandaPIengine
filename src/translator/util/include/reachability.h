9fd91f58cd2494353755dab0bb8c66b6d1d21a76
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-01-14 17:03:40 +1100

    reachability analysis

--- src/translator/util/include/reachability.h
+++ src/translator/util/include/reachability.h
@@ -2,14 +2,21 @@
 #define _reachability_inc_h_
 
 #include "Model.h"
+#include "assert.h"
 
 class TaskReachability {
     private:
+        Model *htn;
         vector<vector<int>> count;
-        vector<int> dfs(int c, vector<bool> &visited);
+        void dfs(int c, vector<bool> &visited);
 
     public:
         TaskReachability(Model *htn);
+        int getActionNum(int c, int a) {
+            assert(this->htn->isPrimitive[a] && !this->htn->isPrimitive[c]);
+            int offset = this->htn->numActions;
+            return this->count[c - offset][a];
+        }
 };
 
 #endif
--- src/translator/util/src/reachability.cpp
+++ src/translator/util/src/reachability.cpp
@@ -2,12 +2,42 @@
 #include "assert.h"
 
 TaskReachability::TaskReachability(Model *htn) {
+    this->htn = htn;
     int offset = htn->numActions;
     this->count.resize(htn->numTasks - offset);
     vector<bool> visited;
     visited.assign(htn->numTasks - offset, false);
     for (int c = htn->numActions; c < htn->numTasks; c++) {
         int index = c - offset;
-        this->count[index] = this->dfs(c, visited);
+        this->dfs(c, visited);
     }
+}
+
+void TaskReachability::dfs(int c, vector<bool> &visited) {
+    assert(!htn->isPrimitive[c]);
+    int offset = htn->numActions;
+    if (visited[c]) return;
+    visited[c] = true;
+    vector<int> result;
+    result.assign(htn->numActions, -1);
+    for (int m = 0; m < htn->numMethodsForTask[c]; m++) {
+        vector<int> local;
+        local.assign(htn->numActions, 0);
+        for (int i = 0; i < htn->numSubTasks[m]; i++) {
+            int t = htn->subTasks[m][i];
+            if (htn->isPrimitive[t]) {
+                local[t] = 1;
+            } else {
+                this->dfs(t, visited);
+                for (int a = 0; a < htn->numActions; a++)
+                    local[a] += this->count[t][a];
+            }
+        }
+        for (int a = 0; a < htn->numActions; a++) {
+            if (result[a] == -1) {
+                result[a] = local[a];
+            } else {result[a] = min(local[a], result[a]);}
+        }
+    }
+    this->count[c] = result;
 }
