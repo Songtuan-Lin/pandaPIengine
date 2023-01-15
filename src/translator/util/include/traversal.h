c223d546d05f899122060c9fe9051342fe180c8d
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-01-15 12:42:54 +1100

    add counting reachable tasks

--- src/translator/util/include/traversal.h
+++ src/translator/util/include/traversal.h
@@ -4,18 +4,24 @@
 #include "Model.h"
 #include "assert.h"
 
-class TaskReachability {
+class TaskTraversal {
     private:
         Model *htn;
         vector<vector<int>> count;
+        vector<int> numReachableTasks;
         void dfs(int c, vector<bool> &visited);
 
     public:
-        TaskReachability(Model *htn);
+        TaskTraversal(Model *htn);
         int getActionNum(int c, int a) {
             assert(this->htn->isPrimitive[a] && !this->htn->isPrimitive[c]);
             int offset = this->htn->numActions;
             return this->count[c - offset][a];
+        }
+        int getNumReachable(int t) {
+            if (this->htn->isPrimitive[t]) return 1;
+            int offset = this->htn->numActions;
+            return this->numReachableTasks[t - offset];
         }
 };
 
--- src/translator/util/src/traversal.cpp
+++ src/translator/util/src/traversal.cpp
@@ -1,10 +1,11 @@
-#include "reachability.h"
+#include "traversal.h"
 #include "assert.h"
 
-TaskReachability::TaskReachability(Model *htn) {
+TaskTraversal::TaskTraversal(Model *htn) {
     this->htn = htn;
     int offset = htn->numActions;
     this->count.resize(htn->numTasks - offset);
+    this->numReachableTasks.resize(htn->numTasks - offset);
     vector<bool> visited;
     visited.assign(htn->numTasks - offset, false);
     for (int c = htn->numActions; c < htn->numTasks; c++) {
@@ -13,24 +14,28 @@ TaskReachability::TaskReachability(Model *htn) {
     }
 }
 
-void TaskReachability::dfs(int c, vector<bool> &visited) {
+void TaskTraversal::dfs(int c, vector<bool> &visited) {
     assert(!htn->isPrimitive[c]);
     int offset = htn->numActions;
     if (visited[c]) return;
     visited[c] = true;
     vector<int> result;
+    int num = -1;
     result.assign(htn->numActions, -1);
     for (int m = 0; m < htn->numMethodsForTask[c]; m++) {
         vector<int> local;
+        int n = 0;
         local.assign(htn->numActions, 0);
         for (int i = 0; i < htn->numSubTasks[m]; i++) {
             int t = htn->subTasks[m][i];
             if (htn->isPrimitive[t]) {
                 local[t] = 1;
+                n += 1;
             } else {
                 this->dfs(t, visited);
                 for (int a = 0; a < htn->numActions; a++)
                     local[a] += this->count[t][a];
+                n += this->numReachableTasks[t];
             }
         }
         for (int a = 0; a < htn->numActions; a++) {
@@ -38,6 +43,10 @@ void TaskReachability::dfs(int c, vector<bool> &visited) {
                 result[a] = local[a];
             } else {result[a] = min(local[a], result[a]);}
         }
+        if (num == -1) {
+            num = n;
+        } else {num = min(num, n);}
     }
     this->count[c] = result;
+    this->numReachableTasks[c] = num;
 }
