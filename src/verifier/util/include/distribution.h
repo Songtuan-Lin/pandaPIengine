1ed1c9a4ea5d04ac78eec0db59ec5e2bb4790a44
Songtuan Lin <songtuan.lin@anu.edu.au>
2023-04-17 17:08:42 +1000

    length distributions

---
+++ src/verifier/util/include/distribution.h
@@ -0,0 +1,35 @@
+//
+// Created by u6162630 on 4/17/23.
+//
+
+#ifndef PANDAPIENGINE_DISTRIBUTION_H
+#define PANDAPIENGINE_DISTRIBUTION_H
+
+#include <climits>
+#include "vector"
+
+using namespace std;
+class LengthDistributions {
+public:
+    LengthDistributions(int total, int numCols) {
+        this->numCols = numCols;
+        this->distribution = new int*[INT_MAX];
+        this->numRows = this->generate(total, 0, 0);
+        this->row = 0;
+    }
+    int* next() {
+        if (this->row >= this->numRows)
+            return nullptr;
+        int r = this->row;
+        this->row++;
+        return this->distribution[r];
+    }
+    int numDistributions() {return this->numRows;}
+private:
+    int** distribution;
+    int row;
+    int numCols;
+    int numRows;
+    int generate(int total, int row, int col);
+};
+#endif //PANDAPIENGINE_DISTRIBUTION_H
